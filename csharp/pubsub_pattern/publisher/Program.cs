7aaeee31bba0ece4f37442ea535d485326ff6809
Mohd Redzuan, Sharizan <Sharizan.MohdRedzuan@hess.com>
2024-10-02 11:08:50 +0800

    Learning PubSub and c# sample code

---
+++ csharp/pubsub_pattern/publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// for PubSub, Publisher/Producer need to declare a Exchange
+channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
+
+// For PubSub, Producer/Publisher dont have to declare a queue
+// channel.QueueDeclare(
+//     queue: "letterbox",
+//     durable: false,
+//     exclusive: false,
+//     autoDelete: false,
+//     arguments: null);
+
+var message = "Hello, I want to broadcast this message";
+var body = Encoding.UTF8.GetBytes(message);
+
+// Publish to Exchange
+channel.BasicPublish(exchange: "pubsub", "", null, body);
+
+Console.WriteLine($"Send Message: {message}");
+
---
+++ csharp/pubsub_pattern/subscriber1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Declare Exchange
+channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
+
+// Use temporary Queue
+// No need for arguments pass
+var queueName = channel.QueueDeclare().QueueName;
+
+var consumer = new EventingBasicConsumer(channel);
+
+// If don't bind, it will not listen for a message from the Queue
+channel.QueueBind(queue: queueName, exchange: "pubsub", routingKey: "");
+
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Subscriber 1: Received new message: {message}");
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine("Consuming...");
+
+Console.ReadKey();
